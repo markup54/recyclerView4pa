016e4475cf25544796147ca0761b0ae47f0d2cb7
markup54 <marzenakupka@tlen.pl>
2024-10-08 08:39:49 +0200

    adapter

--- app/src/main/java/pl/zabrze/zs10/listy4pa/MainActivityRecyclerView.java
+++ app/src/main/java/pl/zabrze/zs10/listy4pa/MainActivityRecyclerView.java
@@ -1,6 +1,8 @@
 package pl.zabrze.zs10.listy4pa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -8,6 +10,8 @@ import java.util.ArrayList;
 
 public class MainActivityRecyclerView extends AppCompatActivity {
     ArrayList<Produkt> produkty = new ArrayList<>();
+    RecyclerView recyclerView;
+    ZakupyAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +22,13 @@ public class MainActivityRecyclerView extends AppCompatActivity {
         produkty.add(new Produkt("woda",2.5,"napoje"));
         produkty.add(new Produkt("krewetki",25.5,"owoce morza"));
         produkty.add(new Produkt("pstrąg",25.5,"ryby"));
+        produkty.add(new Produkt("pstrąg",25.5,"ryby"));
+        produkty.add(new Produkt("pstrąg",25.5,"ryby"));
+        produkty.add(new Produkt("pstrąg",25.5,"ryby"));
+        produkty.add(new Produkt("pstrąg",25.5,"ryby"));
+        recyclerView = findViewById(R.id.recyclerView);
+        adapter = new ZakupyAdapter(this,produkty);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/pl/zabrze/zs10/listy4pa/ZakupyAdapter.java
+++ app/src/main/java/pl/zabrze/zs10/listy4pa/ZakupyAdapter.java
@@ -1,31 +1,54 @@
 package pl.zabrze.zs10.listy4pa;
 
+
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class ZakupyAdapter extends RecyclerView.Adapter<ZakupyAdapter.ProduktViewHolder> {
+    private LayoutInflater inflater;
+    private ArrayList<Produkt> listaZakupow;
+
+    public ZakupyAdapter(Context context,ArrayList<Produkt> listaZakupow) {
+        inflater = LayoutInflater.from(context);
+        this.listaZakupow = listaZakupow;
+    }
+
     @NonNull
     @Override
     public ProduktViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        //przygotowanie widoku
+        View itemView = inflater.inflate(R.layout.produkt_widok_elementu,
+                parent,false);
+        return new ProduktViewHolder(itemView,this);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProduktViewHolder holder, int position) {
-
+        //wypełnienie widoku danymi
+        holder.checkBox.setText(listaZakupow.get(position).getNazwa());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        //ile elementów do wyświetlenia
+        return listaZakupow.size();
     }
 
     public class ProduktViewHolder extends RecyclerView.ViewHolder {
-        public ProduktViewHolder(@NonNull View itemView) {
+        public CheckBox checkBox;
+        public ZakupyAdapter adapter;
+        public ProduktViewHolder(@NonNull View itemView, ZakupyAdapter adapterZ) {
             super(itemView);
+            checkBox = itemView.findViewById(R.id.checkBox);
+            adapter = adapterZ;
         }
     }
 }
